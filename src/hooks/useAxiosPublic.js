8fa4138da20ac2a14ce40a7e00c69778d9cffa75
ushanasif <ushanasif@gmail.com>
2025-03-30 19:58:51 +0600

    new update

--- src/hooks/useAxiosPublic.js
+++ src/hooks/useAxiosPublic.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-console.log(import.meta.env);
+
 const axiosPublic = axios.create({
   baseURL: import.meta.env.MODE === 'development'? 'http://localhost:5000' : import.meta.env.VITE_API_BASE_URL
 });
