0c1d9d16c15a9dbe52d96557f47930360ae2794e
ushanasif <ushanasif@gmail.com>
2025-06-24 19:43:26 +0600

    update now

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -91,27 +91,7 @@ const Login = () => {
                 <div className="absolute inset-0 flex items-center">
                   <div className="w-full border-t border-gray-300"></div>
                 </div>
-                <div className="relative flex justify-center text-sm">
-                  <span className="px-2 bg-gray-100 text-gray-500">
-                    Or continue with
-                  </span>
-                </div>
-              </div>
-  
-              <div className="mt-6">
-                <div className="">
-                  <button
-                    className="w-full flex gap-2 items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
-                  >
-                    
-                    <img
-                      className="h-6 w-6"
-                      src="https://www.svgrepo.com/show/506498/google.svg"
-                      alt=""
-                    />
-                    <p>Sign In With Google</p>
-                  </button>
-                </div>
+              
               </div>
             </div>
           </div>
